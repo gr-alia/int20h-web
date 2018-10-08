dd628944bfbe96163f08877bded603e3a9a7953d
alyona-k <kalytenko.alyona@gmail.com>
2018-10-08 17:25:46 +0300

    Add another gallery lib

--- components/pageHeader/pageHeader.js
+++ components/pageHeader/pageHeader.js
@@ -1,7 +1,16 @@
 var $ = require('jquery');
+require('chocolat');
 
 $(document).ready(function(){
 	console.log('pageHeader script loaded'); // eslint-disable-line no-console
+	$('.gallery-last').Chocolat({
+			imageSelector     : '.chocolat-image',
+			loop           : true,
+  			fullWindow     : 'cover',
+ 			overlayOpacity : 0.9			
+		}
+	);
 
+	
 			
 });
