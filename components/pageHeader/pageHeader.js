76371fd1e2da3b9c19f10056972be64a6f49dad7
alyona-k <kalytenko.alyona@gmail.com>
2018-10-08 16:26:36 +0300

    Remove unitegallery

--- components/pageHeader/pageHeader.js
+++ components/pageHeader/pageHeader.js
@@ -1,11 +1,7 @@
 var $ = require('jquery');
-require('unitegallery');
 
 $(document).ready(function(){
 	console.log('pageHeader script loaded'); // eslint-disable-line no-console
-	jQuery("#gallery").unitegallery({
-		tiles_type:"nested"		
-		}
-	); 
+
 			
 });
