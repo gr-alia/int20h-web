8534cde10eff332ec378c5e0bcdd7646612a5a9a
Alona Kalytenko <alia-sun@meta.ua>
2018-10-10 00:38:14 +0300

    Add contacts carousel

--- components/pageHeader/pageHeader.js
+++ components/pageHeader/pageHeader.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 require('chocolat');
+require('slick-carousel');
 
 $(document).ready(function(){
 	$('.pageLast-photos').Chocolat({
@@ -7,4 +8,23 @@ $(document).ready(function(){
 		loop: true
 	}
 	);
+	$('.contactsContainer').slick({
+		// normal options...
+		infinite: false,
+
+		// the magic
+		responsive: [{
+			breakpoint: 21000,
+			settings: {
+				slidesToShow: 3,
+			}
+		}, {
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				dots: true,
+				infinite: true
+			}
+		}]
+	});
 });
