b0b1772843b7a9c480775c79f0a72080205b9df6
Alyo <jamaica.k0@gmail.com>
2018-10-17 14:05:11 +0300

    Fix place gallery and contacts

--- components/pagePlace/pagePlace.js
+++ components/pagePlace/pagePlace.js
@@ -1,6 +1,12 @@
 var $ = require('jquery');
 require('../../scripts/libs/colcarou.js'); 
 
+var initPlaceGallery = function() {
+	if ($(window).width() >= 992) {
+		$('.placeContainer').colcarou();
+	}
+};
+
 $(document).ready(function(){
-	$('.placeContainer').colcarou();
+	initPlaceGallery();
 });
